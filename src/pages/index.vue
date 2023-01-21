41ba0ef3049687f99734335bf95a5d985b806fcf
nak <nak@home.local>
2023-01-21 21:14:39 +0900

    first commit (2)

---
+++ src/pages/index.vue
@@ -0,0 +1,40 @@
+<script setup lang='ts'>
+const count = ref(0)
+
+onMounted(() => {
+  console.log(process.env.NODE_ENV)
+})
+
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="6">
+            <v-card>
+              <v-card-title>
+                Sample
+              </v-card-title>
+              <v-card-text>
+                <v-text-field v-model="count" variant="outlined" density="compact" label="Count" />
+                <v-btn class="ma-2" color="primary" @click="count++">
+                  <v-icon dark right>
+                    mdi-checkbox-marked-circle
+                  </v-icon>
+                  Accept
+                </v-btn>
+                <v-btn class="ma-2" dark @click="count = 0">
+                  <v-icon dark left>
+                    mdi-minus-circle
+                  </v-icon>Cancel
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,16 @@
+// plugins/vuetify.js
+// https://codybontecou.com/how-to-use-vuetify-with-nuxt-3.html
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    components,
+    directives
+  })
+
+  nuxtApp.vueApp.use(vuetify)
+})
