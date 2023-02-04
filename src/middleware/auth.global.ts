bc7fc80358f060bdc9f143a86c6f4b66ffefb41e
nak <nak@home.local>
2023-02-04 16:11:10 +0900

    updated module, cognito

--- src/middleware/auth.global.ts
+++ src/middleware/auth.global.ts
@@ -1,23 +1,23 @@
 import { Auth } from '@aws-amplify/auth'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  if (to.path == '/logout') {
-    await Auth.signOut()
-    return navigateTo('/login')
-  }
+    if (to.path == '/logout') {
+        await Auth.signOut()
+        return navigateTo('/login')
+    }
 
-  try {
-    const currentAuthUser = await Auth.currentAuthenticatedUser()
-    const session = await Auth.userSession(currentAuthUser)
-    // console.log("[auth]", currentAuthUser)
-    if (!session?.isValid()) {
-      console.log("[auth] Session is invalid")
-      return navigateTo('/login')
+    try {
+        const currentAuthUser = await Auth.currentAuthenticatedUser()
+        const session = await Auth.userSession(currentAuthUser)
+        // console.log("[auth]", currentAuthUser)
+        if (!session?.isValid()) {
+            //      console.log("[auth] Session is invalid")
+            return navigateTo('/login')
+        }
+    } catch (error) {
+        if (to.path != '/login') {
+            //      console.log("[auth] Forbidden")
+            return navigateTo('/login')
+        }
     }
-  } catch (error) {
-    if (to.path != '/login') {
-      console.log("[auth] Forbidden")
-      return navigateTo('/login')
-    }
-  }
 })
